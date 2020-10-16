dfcb8451751e8685aa3fdbfbe911ce107c7e0528
synergit <syner.wang@gmail.com>
2020-10-16 11:10:15 -0400

    removed secret_key

--- geektime/hxnyc/hxnyc/settings.py
+++ geektime/hxnyc/hxnyc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import yaml
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fl3+s2xm-&cwk56r-3ydg+077yh-q31v(i&0sj4o@_3ss2&68+'
+
+with open('./hxnyc/config.yaml') as file:
+    documents = yaml.full_load(file)
+    SECRET_KEY = documents['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
